93eabfdc145998a0386bfef89dcfba7ada360e2c
John Richardson <jorichardson@tsys.com>
2018-04-26 10:36:47 -0400

    Adding some tests for lib/peepers.

---
+++ test/lib/peepers.test.js
@@ -0,0 +1,39 @@
+var expect = require('expect');
+var peepers = require('../../lib/peepers');
+
+describe('Peepers', function() {
+
+  describe('Peepers initialization:', function() {
+    it('Expects Peepers JS import to be a typeof object.', function() {
+      expect(typeof peepers).toBe('object');
+    })
+    it('Expects that constructor contains a results property.', function() {
+      expect(peepers.hasOwnProperty('results')).toBe(true);
+    });
+    it('Expects the results property has been uninitalized (set to 0).', function() {
+      expect(peepers.results.length).toBe(0);
+    });
+    it('Expects that constructor contains a threshold property.', function() {
+      expect(peepers.hasOwnProperty('threshold')).toBe(true);
+    });
+    it('Expects results to be an array', function() {
+      expect(Object.prototype.toString.call(peepers.results)).toBe('[object Array]');
+    });
+    it('Expects that the threshold property has been set to the default value.', function() {
+      expect(peepers.threshold).toBe(100);
+    });
+    it('Expects that constructor contains a searchThresholdSurpassed function.', function() {
+      expect(peepers.hasOwnProperty('searchThresholdSurpassed')).toBe(true);
+    })
+    it('Expects searchThresholdSurpassed to be a typeof function', function() {
+      expect(typeof peepers.searchThresholdSurpassed).toBe('function');
+    });
+    it('Expects that constructor contains a generateRandomTimeout property.', function() {
+      expect(peepers.hasOwnProperty('generateRandomTimeout')).toBe(true);
+    });
+    it('Expects generateRandomTimeout to be a typeof function.', function() {
+      expect(typeof peepers.generateRandomTimeout).toBe('function');
+    });
+  });
+
+});
