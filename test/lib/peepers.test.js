ec29a16dd810db7b65cb36768cc2d362d55e3970
John Richardson <johnmaster208@gmail.com>
2018-05-08 20:31:38 -0400

    Using wrong expect library. Now using chai expect and adding watch script for local tests.

--- test/lib/peepers.test.js
+++ test/lib/peepers.test.js
@@ -1,49 +1,56 @@
-var expect = require('expect');
 var peepers = require('../../lib/peepers');
+var expect = require('chai').expect;
 var sinon = require('sinon');
 
 describe('Peepers', function() {
 
   describe('Gets initialized which...', function() {
     it('Expects Peepers JS import to be a typeof object.', function() {
-      expect(typeof peepers).toBe('object');
+      expect(peepers).to.be.an('object');
     })
     it('Expects that constructor contains a results property.', function() {
-      expect(peepers.hasOwnProperty('results')).toBe(true);
+      expect(peepers).to.have.property('results');
     });
     it('Expects results to be an array', function() {
-      expect(Object.prototype.toString.call(peepers.results)).toBe('[object Array]');
+      expect(peepers.results).to.be.an('array');
     });
     it('Expects the results property has been uninitalized (set to 0).', function() {
-      expect(peepers.results.length).toBe(0);
+      expect(peepers.results.length).to.equal(0);
     });
     it('Expects that constructor contains a threshold property.', function() {
-      expect(peepers.hasOwnProperty('threshold')).toBe(true);
+      expect(peepers).to.have.property('threshold');
     });
     it('Expects that the threshold property has been set to the default value.', function() {
-      expect(peepers.threshold).toBe(100);
+      expect(peepers.threshold).to.equal(100);
     });
     it('Expects that constructor contains a searchThresholdSurpassed function.', function() {
-      expect(peepers.hasOwnProperty('searchThresholdSurpassed')).toBe(true);
+      expect(peepers).to.have.property('searchThresholdSurpassed');
     })
     it('Expects searchThresholdSurpassed to be a typeof function', function() {
-      expect(typeof peepers.searchThresholdSurpassed).toBe('function');
+      expect(peepers.searchThresholdSurpassed).to.be.a('function');
     });
     it('Expects that constructor contains a generateRandomTimeout property.', function() {
-      expect(peepers.hasOwnProperty('generateRandomTimeout')).toBe(true);
+      expect(peepers).to.have.property('generateRandomTimeout');
     });
     it('Expects generateRandomTimeout to be a typeof function.', function() {
-      expect(typeof peepers.generateRandomTimeout).toBe('function');
+      expect(peepers.generateRandomTimeout).to.be.a('function');
     });
     it('Expects that constructor contains a workingAnim property', function() {
-      expect(peepers.hasOwnProperty('workingAnim')).toBe(true);
+      expect(peepers).to.have.property('workingAnim');
     });
     it('Expects that constructor contains an executeQuery property.', function() {
-      expect(peepers.hasOwnProperty('executeQuery')).toBe(true);
+      expect(peepers).to.have.property('executeQuery');
     });
     it('Expects executeQuery to be a typeof function.', function() {
-      expect(typeof peepers.executeQuery).toBe('function');
+      expect(peepers.executeQuery).to.be.a('function');
+    });
+    it('Expects that constructor contains a printResults property.', function() {
+      expect(peepers).to.have.property('printResults');
+    });
+    it('Expects printResults to be a typeof function.', function() {
+      expect(peepers.printResults).to.be.a('function');
     });
   });
+
 
 });
